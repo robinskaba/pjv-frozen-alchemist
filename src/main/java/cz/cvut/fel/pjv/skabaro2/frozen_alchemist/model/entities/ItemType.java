ea0143ce2dd6d01920ea31d21423d882af259c52
Robin Škába - notebook <robin.skaba@gmail.com>
2025-04-14 15:44:07 +0200

    better item descriptions

--- src/main/java/cz/cvut/fel/pjv/skabaro2/frozen_alchemist/model/entities/ItemType.java
+++ src/main/java/cz/cvut/fel/pjv/skabaro2/frozen_alchemist/model/entities/ItemType.java
@@ -8,24 +8,76 @@ import java.util.Map;
  * Enum representing different types of items in the game.
  */
 public enum ItemType implements Savable, Storable {
-    EmberFlower("Ember Flower", "A flower that burns with a cold flame.", new SaveConfig("EF", "ember_flower.png")),
-    EmptyGlassFlask("Empty Glass Flask", "A flask for holding potions.", new SaveConfig("EGF", "empty_glass_flask.png")),
-    Feather("Feather", "A light and delicate feather.", new SaveConfig("F", "feather.png")),
-    GlacialShard("Glacial Shard", "A shard of pure ice.", new SaveConfig("GS", "glacial_shard.png")),
-    Gunpowder("Gunpowder", "Explosive powder.", new SaveConfig("G", "gunpowder.png")),
-    MysticVine("Mystic Vine", "A vine with magical properties.", new SaveConfig("MV", "mystic_vine.png")),
-    RabbitFoot("Rabbit Foot", "A lucky rabbit's foot.", new SaveConfig("RF", "rabbit_foot.png")),
-    SulfurCrystal("Sulfur Crystal", "A crystal of sulfur.", new SaveConfig("SC", "sulfur_crystal.png")),
-    VolatileSalt("Volatile Salt", "A salt that reacts violently.", new SaveConfig("VS", "volatile_salt.png")),
+    EmberFlower(
+            "Ember Flower",
+            "A flower that burns with a cold flame.",
+            new SaveConfig("EF", "ember_flower.png")
+    ),
+    EmptyGlassFlask(
+            "Empty Glass Flask",
+            "A flask for holding potions.",
+            new SaveConfig("EGF", "empty_glass_flask.png")
+    ),
+    Feather(
+            "Feather",
+            "A light and delicate feather. Makes you feel like flying.",
+            new SaveConfig("F", "feather.png")
+    ),
+    GlacialShard(
+            "Glacial Shard",
+            "A shard of pure ice. Very chilling.",
+            new SaveConfig("GS", "glacial_shard.png")
+    ),
+    Gunpowder(
+            "Gunpowder",
+            "Explosive powder. Be careful with it.",
+            new SaveConfig("G", "gunpowder.png")
+    ),
+    MysticVine(
+            "Mystic Vine",
+            "A vine with magical properties.",
+            new SaveConfig("MV", "mystic_vine.png")
+    ),
+    RabbitFoot(
+            "Rabbit Foot",
+            "Where is the difference between a long jump and flying?",
+            new SaveConfig("RF", "rabbit_foot.png")
+    ),
+    SulfurCrystal(
+            "Sulfur Crystal",
+            "A crystal of sulfur. It smells bad.",
+            new SaveConfig("SC", "sulfur_crystal.png")
+    ),
+    VolatileSalt(
+            "Volatile Salt",
+            "A salt that reacts violently. Handle with care.",
+            new SaveConfig("VS", "volatile_salt.png")
+    ),
 
-    PotionOfMelting("Potion of Melting", "Melts blocks.", new SaveConfig("POM", "potion_of_melting.png"),
-            Map.of(EmberFlower, 1)),
-    PotionOfPulverization("Potion of Pulverization", "Pulverizes obstacles.", new SaveConfig("POP", "potion_of_pulverization.png"),
-            Map.of(Gunpowder, 1)),
-    PotionOfLevitation("Potion of Levitation", "Allows levitation.", new SaveConfig("POL", "potion_of_levitation.png"),
-            Map.of(Feather, 1)),
-    PotionOfFrost("Potion of Frost", "Freezes surroundings.", new SaveConfig("POF", "potion_of_frost.png"),
-            Map.of(GlacialShard, 1));
+    PotionOfMelting(
+            "Potion of Melting",
+            "Some blocks are beginning to melt, this will finish the job.",
+            new SaveConfig("POM", "potion_of_melting.png"),
+            Map.of(EmberFlower, 1)
+    ),
+    PotionOfPulverization(
+            "Potion of Pulverization",
+            "Pulverizes certain obstacles, even a large pile of rubble.",
+            new SaveConfig("POP", "potion_of_pulverization.png"),
+            Map.of(Gunpowder, 1)
+    ),
+    PotionOfLevitation(
+            "Potion of Levitation",
+            "Allows levitation over the deep chasms.",
+            new SaveConfig("POL", "potion_of_levitation.png"),
+            Map.of(Feather, 1)
+    ),
+    PotionOfFrost(
+            "Potion of Frost",
+            "Ever heard of the frozen Bering Strait?",
+            new SaveConfig("POF", "potion_of_frost.png"),
+            Map.of(GlacialShard, 1)
+    );
 
     private final String name;
     private final String description;
@@ -37,7 +89,11 @@ public enum ItemType implements Savable, Storable {
      * @param description A brief description of the item.
      * @param saveConfig The save configuration for the item.
      */
-    ItemType(String name, String description, SaveConfig saveConfig) {
+    ItemType(
+        String name,
+        String description,
+        SaveConfig saveConfig
+    ) {
         this.name = name;
         this.description = description;
         this.saveConfig = saveConfig;
@@ -50,7 +106,12 @@ public enum ItemType implements Savable, Storable {
      * @param saveConfig The save configuration for the item.
      * @param recipe The crafting recipe for the item.
      */
-    ItemType(String name, String description, SaveConfig saveConfig, Map<ItemType, Integer> recipe) {
+    ItemType(
+        String name,
+        String description,
+        SaveConfig saveConfig,
+        Map<ItemType, Integer> recipe
+    ) {
         this.name = name;
         this.description = description;
         this.saveConfig = saveConfig;
